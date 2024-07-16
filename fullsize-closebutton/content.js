2c525bbf1a5e9a7666855d6069af6f3be26a1192
ocami <66419581+ocamimic@users.noreply.github.com>
2024-07-16 23:07:13 +0900

    Update content.js

--- fullsize-closebutton/content.js
+++ fullsize-closebutton/content.js
@@ -1,1 +1,2 @@
-document.querySelectorAll(".rate-points span.number").forEach(e => e.textContent = "???");
+var elem = document.querySelectorAll("a.ui-dialog-titlebar-icon.ui-dialog-titlebar-close.ui-corner-all");
+elem.forEach(function (elem) {elem.style.cssText = "top: 0; left: 0; position: fixed; display: block; width: 100%; height: 100%; background: rgba(0,0,0,.2);";}
