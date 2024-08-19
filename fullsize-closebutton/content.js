5fcdd79b3c51583f4a0184c99e4f105bcac1b5f4
ocami <66419581+ocamimic@users.noreply.github.com>
2024-08-19 16:41:01 +0900

    Delete fullsize-closebutton directory

--- fullsize-closebutton/content.js
+++
@@ -1,2 +0,0 @@
-var elem = document.querySelectorAll("a.ui-dialog-titlebar-icon.ui-dialog-titlebar-close.ui-corner-all");
-elem.forEach(function (elem) {elem.style.cssText = "top: 0; left: 0; position: fixed; display: block; width: 100%; height: 100%; background: rgba(0,0,0,.2);";}
